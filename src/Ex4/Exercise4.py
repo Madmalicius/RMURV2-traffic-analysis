236ea488f83fae1bfa453b35635557df05b9919f
rasmus k. henriksen <rasmus@hvilshoej.dk>
2020-04-15 13:29:24 +0200

    add function description

--- src/Ex4/Exercise4.py
+++ src/Ex4/Exercise4.py
@@ -2,6 +2,12 @@ import cv2
 import numpy as np
 
 def perspective_transform(image):
+    """Warps image to birdseye perspective
+
+    Arguments:
+        image {image} -- image or frame to be warped
+
+    """
 
     googleCoords = np.array([[185,308],[774,327],[557,575],[151,479]], dtype="float32") # Order of points: grey house, red house, red shag, grey shag
     stillCoords = np.array([[464,467],[566,196],[823,280],[752,543]], dtype="float32")# Order of points: grey house, red house, red shag, grey shag
