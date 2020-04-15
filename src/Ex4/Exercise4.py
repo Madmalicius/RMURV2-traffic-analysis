a6ec648bff6b955246fa890c8d0def3418fff93b
rasmus k. henriksen <rasmus@hvilshoej.dk>
2020-04-15 12:50:07 +0200

    made perspective transform into function

--- src/Ex4/Exercise4.py
+++ src/Ex4/Exercise4.py
@@ -1,20 +1,19 @@
 import cv2
 import numpy as np
 
+def perspective_transform(image):
 
-# Load images
-stillImg = cv2.imread("still.jpg")
-stillImgPaint = cv2.imread("still_mask.jpg")
-googleImg = cv2.imread("google.png")
-googleImgPaint = cv2.imread("google_mask.png")
+    googleCoords = np.array([[185,308],[774,327],[557,575],[151,479]], dtype="float32") # Order of points: grey house, red house, red shag, grey shag
+    stillCoords = np.array([[464,467],[566,196],[823,280],[752,543]], dtype="float32")# Order of points: grey house, red house, red shag, grey shag
 
-googleCoords = np.array([[185,308],[774,327],[557,575],[151,479]], dtype="float32") # Order of points: grey house, red house, red shag, grey shag
-stillCoords = np.array([[464,467],[566,196],[823,280],[752,543]], dtype="float32")# Order of points: grey house, red house, red shag, grey shag
+    M = cv2.getPerspectiveTransform(stillCoords, googleCoords)
 
-M = cv2.getPerspectiveTransform(stillCoords, googleCoords)
+    warped = cv2.warpPerspective(image, M, (1280, 720))
 
-warped = cv2.warpPerspective(stillImg, M, (1280, 720))
-cv2.imshow("Warped", warped)
-cv2.imwrite("warpedStill.jpg", warped)
+    return warped
+
+imgWarp = perspective_transform(cv2.imread("still.jpg"))
+cv2.imshow("Warped", imgWarp)
+cv2.imwrite("warpedStill.jpg", imgWarp)
 
 cv2.waitKey(0)
